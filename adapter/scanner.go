777365b1308f325b2b9bc438d91f75c62d388279
Jason Younker <jason@ynkr.org>
2024-12-23 13:11:00 -0500

    basic functionality working. This will take input from STDIN, output the stack (aka target aka destination) to STDOUT and any errors to STDERR. ctrl-c will kill it (there is no proper signal handling)

--- adapter/scanner.go
+++ adapter/scanner.go
@@ -4,22 +4,75 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
+
+	"github.com/younker/cribl/model"
 )
 
-type Scanner struct{}
+type Scanner struct {
+	input  io.Reader
+	output io.Writer
+	outerr io.Writer
+}
 
-func NewScanner() Scanner {
-	return Scanner{}
+func NewScanner(in io.Reader, out io.Writer, oerr io.Writer) Scanner {
+	return Scanner{
+		input:  in,
+		output: out,
+		outerr: oerr,
+	}
 }
 
 func (s Scanner) Sort() {
-	scanner := bufio.NewScanner(io.Reader)
+	scanner := bufio.NewScanner(s.input)
 	for scanner.Scan() {
 		input := scanner.Text()
-		fmt.Printf("input: %s\n", input)
-		// parse input -> normalize to model objects
-		// validate input
-		// send to dispatch
-		// print stack
+		pkg, err := csvInputToPackage(input)
+		if err != nil {
+			fmt.Println(s.outerr, fmt.Errorf("unable to parse input '%s' due to: %w", input, err))
+			continue
+		}
+
+		fmt.Fprintln(s.output, model.Dispatch(pkg))
 	}
+}
+
+func csvInputToPackage(line string) (model.Package, error) {
+	// The specific traits of this package are provided as a CSV input where the
+	// values are ordered as follows:
+	//   mass,height,width,depth
+	traits := strings.Split(line, ",")
+	if len(traits) != 4 {
+		return model.Package{}, fmt.Errorf("inputs should contain 4 values 'mass,height,width,depth' in that order. We received invalid input '%s'", line)
+	}
+
+	mass, err := strconv.ParseFloat(traits[0], 32)
+	if err != nil {
+		return model.Package{}, err
+	}
+
+	height, err := strconv.Atoi(traits[1])
+	if err != nil {
+		return model.Package{}, err
+	}
+
+	width, err := strconv.Atoi(traits[2])
+	if err != nil {
+		return model.Package{}, err
+	}
+
+	depth, err := strconv.Atoi(traits[3])
+	if err != nil {
+		return model.Package{}, err
+	}
+
+	return model.Package{
+		Mass: model.Mass(mass),
+		Dimensions: model.Dimensions{
+			Height: height,
+			Width:  width,
+			Depth:  depth,
+		},
+	}, nil
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/younker/cribl/adapter"
 )
 
 func main() {
-	sorter := adapter.NewScanner()
+	sorter := adapter.NewScanner(os.Stdin, os.Stdout, os.Stderr)
 	sorter.Sort()
 }
